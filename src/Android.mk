LineageOS/android_hardware_intel_img_psb_video

intel: psb_video: Add support for LP blobs
The gralloc interface has changed to add a new method which was
previously magically buried in a "reserved" pointer.  Restore the
LP version of the code to support our older blobs.

Change-Id: Ib4f812a6d0efdcc528fa35ec0b1dc38ae149d1cb

src/Android.mk
@@ -160,6 +160,10 @@ LOCAL_CFLAGS += \
     -DPSBVIDEO_MSVDX_DEC_TILING -DPSBVIDEO_MSVDX_EC
 endif
 
+ifeq ($(INTEL_HWC_MOOREFIELD_HAS_LP_BLOBS),true)
+    LOCAL_CFLAGS += -DHAS_LP_BLOBS
+endif
+
 ifeq ($(TARGET_HAS_MULTIPLE_DISPLAY),true)
 LOCAL_SRC_FILES += android/psb_mds.cpp
 LOCAL_CFLAGS += -DTARGET_HAS_MULTIPLE_DISPLAY

AOSP-DEV-ZF2/platform_hardware_intel_img_psb_video

src/Android.mk
@@ -160,6 +160,10 @@ LOCAL_CFLAGS += \
     -DPSBVIDEO_MSVDX_DEC_TILING -DPSBVIDEO_MSVDX_EC
 endif
 
+ifeq ($(INTEL_HWC_MOOREFIELD_HAS_LP_BLOBS),true)
+    LOCAL_CFLAGS += -DLP_BLOBS
+endif
+
 ifeq ($(TARGET_HAS_MULTIPLE_DISPLAY),true)
 LOCAL_SRC_FILES += android/psb_mds.cpp
 LOCAL_CFLAGS += -DTARGET_HAS_MULTIPLE_DISPLAY